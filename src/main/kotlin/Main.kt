28d1a1785ba62a1916cc1353e14e78de3ade5427
Felix Lara <larafelix2197@gmail.com>
2025-06-14 23:40:21 -0500

    in line if else expresion

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,12 +2,11 @@ fun main() {
     val num1 = -5
     val num2 = -4
 
-   val text = if (num1 > 0 || num2 > 0){
-        "The condition is true"
-    } else if (num1 < 0 || num2 < 0) {
-        "The condition is false"
-    } else {
-        "The condition is other"
-   }
+    var text=
+        if (num1 > 0 || num2 > 0) "The condition is true" else if (num1 < 0 || num2 < 0) "The condition is false" else "The condition is other"
+    println("$text")
+
+    text =
+        if (num1 > 0 || num2 > 0) "The condition is true"  else "The condition is other"
     println("$text")
 }
