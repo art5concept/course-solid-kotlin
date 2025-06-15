e1bd1f092fabd0a4467f7c89c45bda9f89a4eb5d
Felix Lara <larafelix2197@gmail.com>
2025-06-14 20:17:09 -0500

    Otra forma de poner floats

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
 fun main() {
 
     // si se quiere que sea float se debe usar asi
-    val myNumber: Float = 2.5f
+    val myNumber = 3.5f
 
     // de forma default los decimales son double
 
-    val mySecondNumber = 2.5
+    val mySecondNumber = 4.5
 
     println("myNumber = $myNumber and mySecondNumber = $mySecondNumber")
 }
