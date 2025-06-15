864bcf5302ff269542a40f440ae0acd4899516d1
Felix Lara <larafelix2197@gmail.com>
2025-06-14 19:46:31 -0500

    variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
 fun main() {
-    println("Hello, World!")
+    var userName = "Alex"
+    userName = "John"
+
+    var age = 22
+
+    age = 13
+
+
+
+    println("Hello $userName your age is $age!")
 }
