b85737043fa6a59cd78879efeb9b43f2a65e83b4
Felix Lara <larafelix2197@gmail.com>
2025-06-15 12:40:59 -0500

    passing indefinite number of parameters using vararg and foreach

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,8 @@
 fun main() {
-    println(sum(1, 2, 3, 4,1,2,3,4,1,2,3,4))
+    sum(1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4)
 
 }
 
-fun sum(vararg numbers: Int): Int {
-    var result = 0
-    for (number in numbers){
-        result += number
-    }
-    return result
+fun sum(vararg numbers: Int) {
+    numbers.forEach { println(it) }
 }
