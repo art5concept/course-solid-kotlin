d0c964b80f07cf1a51b44b4411fd79df92e4cfba
Felix Lara <larafelix2197@gmail.com>
2025-06-14 20:39:44 -0500

    Operaciones con int

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,13 @@
 fun main() {
     // Operators
-
+    // haciendo calculos con integers
     val x = 5
     val y = 3
 
-    val result = x + y
 
-    println("result = $result")
+    println("x + y = ${x + y}")
+    println("x - y = ${x - y}")
+    println("x * y = ${x * y}")
+    println("x / y = ${x / y}")
+    println("x % y = ${x % y}")
 }
