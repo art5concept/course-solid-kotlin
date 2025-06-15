5f50210e04f85fd320c64eeaddfb55ec3c078d55
Felix Lara <larafelix2197@gmail.com>
2025-06-15 00:25:56 -0500

    'text2 = text ?: 'some text'

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,14 +2,17 @@ fun main() {
     // Null pointer exception
     var text: String? = null
 
-    // usando operador non null asserted !!.
-    println(text!!.length)
+//    text = "no nulo, ultimo maestro airre"
+    val text2 = text ?: "The text is null and i print this."
+
+    println(text2)
+//    Explicacion
+//    si text es nulo se asigna "the text is null...
+//    sino es nulo se asigna el valor de text a text2
+
+
 
     //OUTPUT
-
-
-    //Exception in thread "main" java.lang.NullPointerException
-    //	at MainKt.main(Main.kt:7)
-    //	at MainKt.main(Main.kt)
+    //The text is null and i print this.
 
 }
