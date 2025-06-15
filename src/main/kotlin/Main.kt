48c336a12880cd0cb97b10c71c9937a8d7be1bdf
Felix Lara <larafelix2197@gmail.com>
2025-06-15 13:05:21 -0500

    LOOPS FOR

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,26 @@
 fun main() {
-    sum(1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4)
+    // loops
+    for (i in 1..4) {
+        println("i = $i")
+    }
 
-}
+    println("===============")
 
-fun sum(vararg numbers: Int) {
-    numbers.forEach { println(it) }
+    for (i in 1 until 4) {
+        println("i = $i")
+    }
+
+    println("===============")
+
+    for (i in 4 downTo 1){
+        println("i = $i")
+    }
+
+    println("===============")
+
+    for (i in 1 until 10 step 2){
+        println("i = $i")
+    }
+
+
 }
