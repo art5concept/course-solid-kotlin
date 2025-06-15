e56cda6687447ebef91fb176b62ab7cc6612018f
Felix Lara <larafelix2197@gmail.com>
2025-06-15 01:07:50 -0500

    forma resumida de poner funciones de una linea

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,10 @@
 fun main() {
     val max = getMax(2, 4)
     val min = getMin(2, 4)
+    val min2 = getMin2(2, 4)
     println(max)
     println(min)
+    println(min2)
 }
 
 fun getMax(a: Int, b: Int): Int {
@@ -10,9 +12,16 @@ fun getMax(a: Int, b: Int): Int {
     return max
 }
 
+// forma consisa
+
 fun getMin(a: Int, b: Int): Int {
     return if (a < b) a else b
 }
+
+// aun mas conciso si solo se tiene una linea de codigo en funciones asi
+
+fun getMin2(a: Int, b: Int) = if (a < b) a else b
+
 
 // OUTPUT
 // 4
