2ebc51bdabcce08f65653bc2bb1ef8116f8f8df1
Felix Lara <larafelix2197@gmail.com>
2025-06-15 00:09:51 -0500

    usando only safe operator ?.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,8 @@
 fun main() {
     // Null pointer exception
-    // se debe poner un ? al tipo para que kotlin permita que una variable
-    // tenga dato null porque por defecto no se permite
     var text: String? = "name"
-    text = null
-    if (text != null){
-        println("No es null, es $text")
-    } else {
-        println("Es null, $text")
-    }
 
+    // usando only safe operator ?.
+    println(text?.length)
 
 }
